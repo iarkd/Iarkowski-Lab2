18372898b0aed2c4a10e73bc2b99173510dc571d
unknown <4070124@HS222-10.vtsd.com>
2015-09-22 11:54:04 -0400

    My First Commit Because I Was Told To Write This

---
+++ Lab02Cv100.java
@@ -0,0 +1,24 @@
+/* Lab02Cv100.java
+Bananas. 42. That is all. */
+
+public class Lab02Cv100
+{
+    public static void main(String arch[])
+    {
+        System.out.println("Lab 02C, 100 Point Version");
+        System.out.println();
+        System.out.println("First Java Program");
+        System.out.println();
+        System.out.print("Measure today's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("# ##### #####    ##### ##### # #   # ##### #####");
+        System.out.println("# #   # #   #    #   # #   # # ##  #   #   #    ");
+        System.out.println("# #   # #   #    ##### #   # # # # #   #   #####");
+        System.out.println("# #   # #   #    #     #   # # #  ##   #       #");
+        System.out.println("# ##### #####    #     ##### # #   #   #   #####");
+    }
+}
+        
